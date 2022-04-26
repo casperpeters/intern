421c0f6a7ea8725cf5d3348530d0bb48fa31e19d
Casper <casper.peters@gmail.com>
2022-04-26 13:38:48 +0200

    added gaussian wave form poisson_data

--- data/poisson_data.py
+++ data/poisson_data.py
@@ -1,7 +1,7 @@
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
+from sklearn.neighbors import KernelDensity
 
 
 class PoissonTimeShiftedData(object):
@@ -12,19 +12,31 @@ class PoissonTimeShiftedData(object):
             n_batches=200,
             time_steps_per_batch=100,
             delay=1,
+            population_wave_form='sin',  # ('sin', 'gaussian')
             temporal_connections=None,
             amplitude_range=None,
             frequency_range=None,
+            n_peaks_range=None,
+            bandwidth=None,
     ):
 
         """
         """
 
         # if no temporal connections are given, take half inhibitory and half exciting populations
-        if frequency_range is None:
-            frequency_range = [2, 5]
-        if amplitude_range is None:
-            amplitude_range = [.05, .5]
+        if population_wave_form == 'sin':
+            if frequency_range is None:
+                frequency_range = [2, 5]
+            if amplitude_range is None:
+                amplitude_range = [.05, .5]
+        elif population_wave_form == 'gaussian':
+            if n_peaks_range is None:
+                n_peaks_range = [2, 10]
+            if bandwidth is None:
+                bandwidth = 5
+            if amplitude_range is None:
+                amplitude_range = [.1, .6]
+
         if temporal_connections is None:
             temporal_connections = torch.cat(
                 tensors=(torch.ones(n_populations, n_populations // 2),
@@ -32,6 +44,7 @@ class PoissonTimeShiftedData(object):
                 dim=1
             ) / (n_populations * 2)
 
+        self.time_steps_per_batch = time_steps_per_batch
         self.t = torch.linspace(start=0, end=2 * torch.pi, steps=time_steps_per_batch)
         neuron_indexes = torch.arange(end=neurons_per_population * n_populations).view(n_populations,
                                                                                        neurons_per_population)
@@ -53,12 +66,19 @@ class PoissonTimeShiftedData(object):
 
             # get all mother trains by looping over populations
             for population_index in range(n_populations):
-                # get a random sine wave as mother train firing rate
-                population_wave = self.get_random_sine(
-                    amplitude_range=amplitude_range,
-                    frequency_range=frequency_range,
-                    phase_range=[0, torch.pi]
-                )
+                if population_wave_form == 'sin':
+                    # get a random sine wave as mother train firing rate
+                    population_wave = self.get_random_sine(
+                        amplitude_range=amplitude_range,
+                        frequency_range=frequency_range,
+                        phase_range=[0, torch.pi]
+                    )
+                elif population_wave_form == 'gaussian':
+                    population_wave = self.get_random_gaussians(
+                        n_peaks_range=n_peaks_range,
+                        bandwidth=bandwidth,
+                        amplitude_range=amplitude_range
+                    )
 
                 # poisson draw mother train
                 mother_train = torch.poisson(population_wave)
@@ -143,6 +163,22 @@ class PoissonTimeShiftedData(object):
 
         return amplitude * torch.sin(frequency * self.t - phase) + amplitude
 
+    def get_random_gaussians(
+            self,
+            n_peaks_range,
+            bandwidth,
+            amplitude_range
+    ):
+        """
+        """
+        amplitude = torch.rand(1) * (amplitude_range[1] - amplitude_range[0]) + amplitude_range[0]
+        n_peaks = np.random.randint(low=n_peaks_range[0], high=n_peaks_range[1])
+        peak_locations = np.random.randint(low=0, high=self.time_steps_per_batch, size=n_peaks)
+        kde = KernelDensity(kernel="gaussian", bandwidth=bandwidth).fit(peak_locations[:, None])
+        gausfit = kde.score_samples(np.arange(start=0, stop=self.time_steps_per_batch)[:, None])
+        wave = torch.tensor(np.exp(gausfit))
+        return wave / torch.max(wave) * amplitude
+
     def plot_stats(self, batch=0, axes=None):
         """
         plots the added, deleted and final spikes of the class of given batch. Also plots the population waves, mother
@@ -159,27 +195,31 @@ class PoissonTimeShiftedData(object):
             fig, axes = plt.subplots(2, 3, figsize=(20, 10))
         elif axes.shape != (2, 3):
             raise ValueError('axes must be None or of shape (2, 3)')
-        sns.heatmap(self.data[..., batch], ax=axes[0, 0], cbar=False)
+        axes[0, 0].imshow(self.data[..., batch], cmap=plt.get_cmap('binary'), aspect='auto')
         axes[0, 0].set_title('Final spikes')
-        sns.heatmap(self.deleted_spikes[..., batch], ax=axes[0, 1], cbar=False)
+        axes[0, 1].imshow(self.deleted_spikes[..., batch], cmap=plt.get_cmap('binary'), aspect='auto')
         axes[0, 1].set_title('Deleted spikes')
-        sns.heatmap(self.added_spikes[..., batch], ax=axes[0, 2], cbar=False)
+        axes[0, 2].imshow(self.added_spikes[..., batch], cmap=plt.get_cmap('binary'), aspect='auto')
         axes[0, 2].set_title('Added spikes')
         for wave in self.population_waves[..., batch]:
             axes[1, 0].plot(wave)
         axes[1, 0].set_title('Population waves')
-        sns.heatmap(self.mother_trains[..., batch], ax=axes[1, 1])
+        axes[1, 1].imshow(self.mother_trains[..., batch], cmap=plt.get_cmap('binary'), aspect='auto')
         axes[1, 1].set_title('Mother trains')
         axes[1, 2].plot(self.firing_rates[torch.argsort(self.firing_rates)], '.')
         axes[1, 2].set_title('Mean firing rates (over all batches)')
         return axes
 
-    def add_noise(self, sigma=.001):
-        add = (torch.rand(self.data.shape) < sigma).type(torch.float)
-        delete = (torch.rand(self.data.shape) < sigma).type(torch.float)
-        self.data = self.data + add - delete
+    def add_noise(self, sigma=.001, range=.5, delete_spikes=True):
+        sigma = torch.linspace(sigma-sigma*range, sigma+sigma*range, self.data.shape[0]).unsqueeze(1).unsqueeze(2)
+        add = (torch.rand(self.data.shape) < sigma.tile(1, self.data.shape[1], self.data.shape[2])).type(torch.float)
+
+        self.data += add
+        if delete_spikes:
+            delete = (torch.rand(self.data.shape) < sigma).type(torch.float)
+            self.deleted_spikes += delete
+            self.data -= delete
         self.added_spikes += add
-        self.deleted_spikes += delete
         self.data[self.data < 0] = 0
         self.data[self.data > 1] = 1
         self.firing_rates = torch.mean(self.data, (1, 2))
@@ -187,7 +227,22 @@ class PoissonTimeShiftedData(object):
 
 
 if __name__ == '__main__':
-    x = PoissonTimeShiftedData(n_batches=1, n_populations=4)
+    temporal_conn = torch.tensor([
+        [0, -1, 1],
+        [1, 0, -1],
+        [-1, 1, 0]
+    ])
+
+    x = PoissonTimeShiftedData(
+        neurons_per_population=7,
+        n_populations=3,
+        n_batches=500,
+        time_steps_per_batch=30,
+        temporal_connections=temporal_conn,
+        amplitude_range=[.05, .3],
+        population_wave_form='gaussian',
+        bandwidth=1,
+    )
     x.add_noise()
     axes = x.plot_stats()
     plt.show()
