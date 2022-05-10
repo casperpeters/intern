32c68c799357bb638d7c00d29e9b70dd60d8afca
casperpeters <casper.peters@gmail.com>
2022-05-10 14:19:49 +0200

    can get split data from chen and thijs now

--- data/load_data.py
+++ data/load_data.py
@@ -22,11 +22,8 @@ def load_data(data_path=None):
     return spikes, behavior, coordinates, df, stimulus
 
 
-def load_data_thijs():
-    path = os.path.dirname(os.getcwd())
-    sys.path.append(path)
-
-    data_obj = h5py.File(path + '/data/real_zebrafish_data/thijs.h5', 'r')
+def load_data_thijs(data_path='/home/sebastian/Desktop/PGM/data/Zebrafish/neural_recordings/full_calcium_data_sets/20180706_Run04_spontaneous_rbm0.h5'):
+    data_obj = h5py.File(data_path, 'r')
     spikes = np.transpose(np.array(data_obj['Data']['Brain']['Analysis']['ThresholdedSpikes']))
     coordinates = np.transpose(np.array(data_obj['Data']['Brain']['Coordinates']))
     times = np.array(data_obj['Data']['Brain']['Times'])
@@ -34,8 +31,13 @@ def load_data_thijs():
     return spikes, coordinates, times
 
 
-def get_split_data(N_V, train_batches=80, test_batches=20):
-    spikes, behavior, coordinates, df, stimulus = load_data('/mnt/data/zebrafish/chen2018/subject_1/Deconvolved/subject_1_reconv_spikes.h5')
+def get_split_data(N_V, train_batches=80, test_batches=20, which='chen'):
+    if which == 'chen':
+        spikes, behavior, coordinates, df, stimulus = load_data(
+            '/mnt/data/zebrafish/chen2018/subject_1/Deconvolved/subject_1_reconv_spikes.h5'
+        )
+    elif which == 'thijs':
+        spikes, coordinates, times = load_data_thijs()
 
     # sort spikes by ascending firing rate
     firing_rates = np.mean(spikes, 1)
